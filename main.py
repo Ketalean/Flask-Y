61c36cb57eca1a9eeeb78e34ab8551cfad845e64
flairy <flairyyluv1@gmail.com>
2025-02-23 19:29:19 +0500

    Изображение Марса

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -24,5 +24,19 @@ def prom():
 ''')
 
 
+@app.route('/image_mars')
+def mars():
+    return (f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='image/MARS.png')}" 
+                    alt="здесь должна была быть картинка, но не нашлась">
+                  </body>''')
+
+
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1', debug=True)
